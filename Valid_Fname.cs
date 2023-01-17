61e7c54d71c34f4c66df6949f66352e012ff8f41
Anshuman202 <asanshuman2@gmail.com>
2023-01-17 12:49:46 +0530

    chacking valid first name

---
+++ Valid_Fname.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RejexAssignment
+{
+    class Program
+    {
+        public static string Reg_pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+
+
+            bool val = Regex.IsMatch(name, Reg_pattern);
+
+
+
+
+
+            if (val)
+            {
+                Console.WriteLine("Pattern is correct for given user name: {0}", name);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
+
+        }
+    }
+}
