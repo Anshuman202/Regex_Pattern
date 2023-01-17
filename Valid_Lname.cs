4a300b89274e4430485e7c2bef2b9fdfcffbd3f3
Anshuman202 <asanshuman2@gmail.com>
2023-01-17 12:59:16 +0530

    validating last name using regex

---
+++ Valid_Lname.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RejexAssignment
+{
+    class Program
+    {
+        public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("enter your First name name");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("enter your Last name name");
+            string lname = Console.ReadLine();
+            Console.WriteLine();
+
+
+
+            bool val1 = Regex.IsMatch(name, Reg_pattern1);
+            bool val2 = Regex.IsMatch(lname, Reg_pattern2);
+
+
+
+
+            if (val1)
+            {
+                Console.WriteLine("Pattern is correct for given First name: {0}", name);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
+            if (val2)
+            {
+                Console.WriteLine("Pattern is correct for given Last name: {0}", lname);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
+
+        }
+    }
+}
